0b00055dbd7f5554fae1e53c30074c1a7987e5a2
jesicaahr <jesicaprilia18@gmail.com>
2020-04-10 20:30:43 +0700

    oauth error

--- main.js
+++ main.js
@@ -2,21 +2,18 @@ const baseUrl = 'http://localhost:3000'
 var app = new Vue ({
     el: '#app',
     data: {
+        id: '',
+        title: '',
+        category: '',
+        UserId: '',
         userData: {
             email: '',
             password: ''
         },
+        tasks: [],
         isLogin: false,
         loginForm: false,
         registForm: false,
-        tasks: [
-            {id: 1, task: "This is kanban (backlog)", category: 'backlog'},
-            {id: 2, task: "This is kanban (todo)", category: 'todo'},
-            {id: 3, task: "This is kanban (done)", category: 'done'},
-            {id: 4, task: "This is kanban (completed)", category: 'completed'},
-            {id: 5, task: "This is kanban (backlog)", category: 'backlog'},
-            {id: 6, task: "This is kanban (done)", category: 'done'},
-        ]
     },
     computed: {
 
@@ -55,13 +52,14 @@ var app = new Vue ({
                     this.isLogin = true
                     this.userData.email = ''
                     this.userData.password = ''
+                    console.log('Successfully registered new account');
+                    
                 })
                 .catch(err=> {
                     console.log(err.response.data.errors[0].message);
                 })
         },
             
-
         login(){
             
             let email= this.userData.email
@@ -76,20 +74,47 @@ var app = new Vue ({
                 }
             })
                 .then(data => {
-                    const access_token = data.access_token
+                    const access_token = data.data.access_token
                     localStorage.setItem('access_token', access_token)
                     this.isLogin = true
                     this.userData.email = ''
-                    this.userData.password = ''
+                    this.userData.password = ''                    
+                    this.readTask()
 
                 })
                 .catch(err=> {
-                    console.log(err.response.data.msg);
+                    err.response == undefined? console.log('Login Success') : console.log(err.response.data.msg);
                 })
         },
 
+        onSignIn(googleUser) {
+            var id_token = googleUser.getAuthResponse().id_token;    
+            console.log(id_token);
+            
+            axios({
+                method: 'POST',
+                url: baseUrl + '/users/googleSign',
+                data: {
+                    'id_token' : id_token,
+                }
+            })
+            
+                .then(data => {
+                    localStorage.setItem('token', data.token)
+                    // console.log("Success login");
+                    this.isLogin = true
+                    this.readTask()
+                })
+                .catch(err => {
+                    console.log(err.responseJSON);
+                })
+        },
+        
         logout(){
             localStorage.clear()
+            // auth2.signOut().then(function() {
+            //     console.log('User signed out.');
+            // });
             this.isLogin = false
         },
 
@@ -98,27 +123,98 @@ var app = new Vue ({
                 method: 'GET',
                 url: baseUrl + '/tasks',
                 headers: {
-                    access_token: localStorage.token
-                }
+                    access_token: localStorage.access_token
+                },
+                
             })
-            .then()
-            .catch()
+            .then(result => {
+                this.tasks = result.data.task
+                console.log(this.tasks);
+                
+            })
+            .catch(err => {
+                console.log(err);
+            })
         },
 
         createTask(){
-            axios({  
+            let title = this.title
+            axios({
                 method: 'POST',
                 url: baseUrl + '/tasks',
                 headers: {
-                    access_token: localStorage.token
+                    access_token: localStorage.access_token
                 },
                 data: {
-                    newTask
+                    title
                 }
             })
-            .then()
-            .catch()
+            .then(result => {
+                console.log("Successfully added new task");
+                this.readTask()
+                
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        },
 
+        deleteTask(id){
+            axios ({
+                method: 'delete',
+                url: baseUrl + '/tasks/'+id,
+                headers: {
+                    access_token: localStorage.access_token
+                }
+            })
+            .then(result => {
+                console.log("Successfully deleted task");
+                this.title = 
+                this.readTask()
+                
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        },
+
+        moveForward(id){
+            axios ({
+                method: 'patch',
+                url: baseUrl + '/tasks/for/'+id,
+                headers: {
+                    access_token: localStorage.access_token
+                }
+            })
+            .then(result => {
+                console.log("Successfully updated task");
+                this.readTask()
+                
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            
+        },
+
+        moveBackward(id){
+            console.log("backward jalan");
+            axios ({
+                method: 'patch',
+                url: baseUrl + '/tasks/back/'+id,
+                headers: {
+                    access_token: localStorage.access_token
+                }
+            })
+            .then(result => {
+                console.log("Successfully updated task");
+                this.readTask()
+                
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            
         }
 
     },
@@ -130,6 +226,7 @@ var app = new Vue ({
         console.log('created')
         if(localStorage.access_token){
             this.isLogin = true
+            this.readTask()
         }
     },
     beforeMount(){
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: "Welcome Kanban"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
