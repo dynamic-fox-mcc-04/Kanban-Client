fb3edf24637a275cf87c0fb203f0f7424104aa97
maxhamz <maxwell.hamzah@gmail.com>
2020-04-10 16:40:49 +0700

    trimming confounds

--- config/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const BASEURL = 'http://localhost:3001'
-// const BASEURL = 'https://desolate-ocean-86590.herokuapp.com'
-
-const instance = axios.create({
-    baseURL: BASEURL
-})
-
-export default instance
--- config/socket.js
+++
@@ -1,6 +0,0 @@
-import io from 'socket.io-client'
-
-const BASEURL = 'http://localhost:3001'
-// const BASEURL = 'https://desolate-ocean-86590.herokuapp.com'
-
-export default io(BASEURL)
