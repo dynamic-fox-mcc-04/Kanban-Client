d25aff6643e30596bff968e42e1586f902d7c810
maxhamz <maxwell.hamzah@gmail.com>
2020-04-10 16:58:07 +0700

    DEPLOY

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const BASEURL = 'http://localhost:3001'
-// const BASEURL = 'https://desolate-ocean-86590.herokuapp.com'
+// const BASEURL = 'http://localhost:3001'
+const BASEURL = 'https://desolate-ocean-86590.herokuapp.com'
 
 const instance = axios.create({
     baseURL: BASEURL
